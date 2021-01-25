442676bb4446d7752c5c76b4a4938ffa7edf73d2
rohan-04 <mathurrohan04@gmail.com>
2021-01-25 19:59:11 +0530

    Add two Dummy component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default class App extends React.Component {
       <BrowserRouter>
         <div className="App">
           <Dummy />
+          <Dummy />
         </div>
       </BrowserRouter>
     );
