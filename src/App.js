ce759a3c14988e282d98a969efbb4d47ca9dfda7
rohan-04 <mathurrohan04@gmail.com>
2021-01-25 19:53:47 +0530

    Add Dummy component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import MainComponent from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Dummy from './components/Dummy';
 
 export default class App extends React.Component {
 
   render() {
     return (
       <BrowserRouter>
-	      <div className="App">
-	        <MainComponent />
-	      </div>
-    </BrowserRouter>
+        <div className="App">
+          <Dummy />
+        </div>
+      </BrowserRouter>
     );
   }
 }
