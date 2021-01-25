19de3c16634c1a8ee177ce075b3b5bc33ac68b2e
rohan-04 <mathurrohan04@gmail.com>
2021-01-25 20:18:37 +0530

    Trail

--- src/App.js
+++ src/App.js
@@ -2,30 +2,21 @@ import React from 'react';
 import MainComponent from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
-import Dummy from './components/Dummy';
-=======
-import Deepak from './components/Deepak.js;
->>>>>>> 9f80f67427a798929ad8719a1fbc9573a9da5e5b
+import Deepak from './components/Deepak.js';
 
 export default class App extends React.Component {
 
   render() {
     return (
       <BrowserRouter>
-<<<<<<< HEAD
-    <div className="App">
-      <Dummy />
-      <Dummy />
-    </div>
-      </BrowserRouter >
-=======
-	      <div className="App">
-	        <MainComponent />
-	    	<Deepak />
-	      </div>
-    </BrowserRouter>
->>>>>>> 9f80f67427a798929ad8719a1fbc9573a9da5e5b
+        <div className="App">
+          <Deepak />
+          <Dummy />
+          <Dummy />
+        </div>
+      </BrowserRouter>
     );
   }
 }
+
+export default App
