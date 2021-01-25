dd1554f0ade63be5c3ba87eb1c7886ef150c6fb2
Deepak-1373 <61752151+Deepak-1373@users.noreply.github.com>
2021-01-25 19:48:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MainComponent from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Deepak from './components/Deepak.js;
 
 export default class App extends React.Component {
 
